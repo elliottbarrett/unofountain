9650e5bf05aff6fb0cb7636759b05e4cd9d1d92a
Elliott Barrett <elliott@cribcut.com>
2019-06-27 22:29:48 -0300

    Got comms working between master and slave

--- PumpMaster/PumpMaster.ino
+++ PumpMaster/PumpMaster.ino
@@ -1,6 +1,91 @@
+const int txEnablePin = 13;
+const int txData0Pin = 2;
+const int txData1Pin = 3;
+const int txData2Pin = 4;
+const int txData3Pin = 5;
+const int txData4Pin = 6;
+const int txData5Pin = 7;
+const int txMode0Pin = 22;
+const int txMode1Pin = 23;
+const int holdTime = 15; // ms
+
+enum txMode {
+  MODE_RED,
+  MODE_GREEN,
+  MODE_BLUE,
+  MODE_PUMP,
+  MODE_INVALID
+};
+
+int rVal;
+int gVal;
+int bVal;
+int pVal;
+unsigned int time = 0;
+unsigned int lastPrint = 0;
+
 void setup() {
   Serial.begin(9600);
+  rVal = 16;
+  gVal = 32;
+  bVal = 48;
+  pVal = 0;
+  time = 0;
+  
+  pinMode(txEnablePin, OUTPUT);
+  pinMode(txData0Pin, OUTPUT);
+  pinMode(txData1Pin, OUTPUT);
+  pinMode(txData2Pin, OUTPUT);
+  pinMode(txData3Pin, OUTPUT);
+  pinMode(txData4Pin, OUTPUT);
+  pinMode(txData5Pin, OUTPUT);
+  pinMode(txMode0Pin, OUTPUT);
+  pinMode(txMode1Pin, OUTPUT);
+}
+
+void writeValue(txMode mode, int val) {
+  digitalWrite(txEnablePin, LOW);
+  digitalWrite(txMode0Pin, (int)mode & 0x01 ? HIGH : LOW);
+  digitalWrite(txMode1Pin, (int)mode & 0x02 ? HIGH : LOW);
+  digitalWrite(txData0Pin, val >> 0 & 0x01 ? HIGH : LOW);
+  digitalWrite(txData1Pin, val >> 1 & 0x01 ? HIGH : LOW);
+  digitalWrite(txData2Pin, val >> 2 & 0x01 ? HIGH : LOW);
+  digitalWrite(txData3Pin, val >> 3 & 0x01 ? HIGH : LOW);
+  digitalWrite(txData4Pin, val >> 4 & 0x01 ? HIGH : LOW);
+  digitalWrite(txData5Pin, val >> 5 & 0x01 ? HIGH : LOW);
+  digitalWrite(txEnablePin, HIGH);
+}
+
+void printState() {
+  Serial.print("(Master) R: ");
+  Serial.print(rVal);
+  Serial.print(", G: ");
+  Serial.print(gVal);
+  Serial.print(", B: ");
+  Serial.print(bVal);
+  Serial.print(", P: ");
+  Serial.print(pVal);
+  Serial.print('\n');
 }
 
 void loop() {
+  time = millis();
+  writeValue(MODE_RED, rVal);
+  delay(holdTime);
+  writeValue(MODE_GREEN, gVal);
+  delay(holdTime);
+  writeValue(MODE_BLUE, bVal);
+  delay(holdTime);
+  writeValue(MODE_PUMP, pVal);
+  delay(holdTime);
+  digitalWrite(txEnablePin, LOW);
+  delay(100 - holdTime * 4);
+  rVal = (rVal + 1) % 64;
+  gVal = (gVal + 1) % 64;
+  bVal = (bVal + 1) % 64;
+  pVal = (pVal + 1) % 64;
+  if (time - lastPrint > 100) {
+    printState();
+    lastPrint = time;
+  }
 }
--- PumpSlave/PumpSlave.ino
+++ PumpSlave/PumpSlave.ino
@@ -1,49 +1,108 @@
-const int rxPumpPin = A0;
-const int rxRedPin = A1;
-const int rxGreenPin = A2;
-const int rxBluePin = A3;
+const int rxEnablePin = 13;
+const int rxData0Pin = A5;
+const int rxData1Pin = A4;
+const int rxData2Pin = A3;
+const int rxData3Pin = A2;
+const int rxData4Pin = A1;
+const int rxData5Pin = A0;
+const int rxMode0Pin = 4;
+const int rxMode1Pin = 2;
+const int txRedPin = 9;
+const int txGreenPin = 10;
+const int txBluePin = 11;
+const int txPumpPin = 6;
 
-const int txPumpPin = 10;
-const int txRedPin = 6;
-const int txGreenPin = 5;
-const int txBluePin = 3;
-const int readEnablePin = 2;
+enum RxMode {
+  MODE_RED,
+  MODE_GREEN,
+  MODE_BLUE,
+  MODE_PUMP,
+  MODE_INVALID
+};
 
-// Constrained to [0,255]
-int pumpVal = 0;
-int redVal = 0;
-int greenVal = 0;
-int blueVal = 0;
+unsigned int rVal = 0;
+unsigned int gVal = 0;
+unsigned int bVal = 0;
+unsigned int pVal = 0;
+RxMode mode = MODE_INVALID;
+
+unsigned int time = 0;
+unsigned int lastPrint = 0;
 
 void setup() {
   Serial.begin(9600);
   
-  pinMode(txPumpPin, OUTPUT);
+  pinMode(rxEnablePin, INPUT);
+  pinMode(rxData0Pin, INPUT);
+  pinMode(rxData1Pin, INPUT);
+  pinMode(rxData2Pin, INPUT);
+  pinMode(rxData3Pin, INPUT);
+  pinMode(rxData4Pin, INPUT);
+  pinMode(rxData5Pin, INPUT);
+  pinMode(rxMode0Pin, INPUT);
+  pinMode(rxMode1Pin, INPUT);
+  
   pinMode(txRedPin, OUTPUT);
   pinMode(txGreenPin, OUTPUT);
   pinMode(txBluePin, OUTPUT);
+  pinMode(txPumpPin, OUTPUT);
+}
+
+void readMode() {
+  int val = (digitalRead(rxMode1Pin) == HIGH ? 1 : 0);
+  val = (val << 1) | (digitalRead(rxMode0Pin) == HIGH ? 1 : 0);
+  mode = (RxMode) val;
+}
+
+void readValue() {
+  unsigned int val = (digitalRead(rxData5Pin) == HIGH ? 1 : 0);
+  val = (val << 1) | (digitalRead(rxData4Pin) == HIGH ? 1 : 0);
+  val = (val << 1) | (digitalRead(rxData3Pin) == HIGH ? 1 : 0);
+  val = (val << 1) | (digitalRead(rxData2Pin) == HIGH ? 1 : 0);
+  val = (val << 1) | (digitalRead(rxData1Pin) == HIGH ? 1 : 0);
+  val = (val << 1) | (digitalRead(rxData0Pin) == HIGH ? 1 : 0);
   
-  pinMode(rxPumpPin, INPUT);
-  pinMode(rxRedPin, INPUT);
-  pinMode(rxGreenPin, INPUT);
-  pinMode(rxBluePin, INPUT);
-  pinMode(readEnablePin, INPUT);
+  switch (mode) {
+    case MODE_RED:
+      rVal = val;
+      break;
+    case MODE_GREEN:
+      gVal = val;
+      break;
+    case MODE_BLUE:
+      bVal = val;
+      break;
+    case MODE_PUMP:
+      pVal = val;
+      break;
+  }
+}
+
+void printState() {
+  Serial.print("(Slave) R: ");
+  Serial.print(rVal);
+  Serial.print(", G: ");
+  Serial.print(gVal);
+  Serial.print(", B: ");
+  Serial.print(bVal);
+  Serial.print(", P: ");
+  Serial.print(pVal);
+  Serial.print('\n');
 }
 
 void loop() {
-  if (digitalRead(readEnablePin) == HIGH) {
-    // analogRead returns 0..1023, need to shift to translate to 0..255 range
-    pumpVal = analogRead(rxPumpPin) >> 2;
-    redVal = analogRead(rxRedPin) >> 2;
-    greenVal = analogRead(rxGreenPin) >> 2;
-    blueVal = analogRead(rxBluePin) >> 2; 
+  time = millis();
+  if (digitalRead(rxEnablePin) == HIGH) {
+    readMode();
+    readValue();
   }
-
-  analogWrite(txPumpPin, pumpVal);
-  analogWrite(txRedPin, redVal);
-  analogWrite(txGreenPin, greenVal);
-  analogWrite(txBluePin, blueVal);
-
-  Serial.print(pumpVal);
-  Serial.print("\n");
+  analogWrite(txRedPin, rVal);
+  analogWrite(txGreenPin, gVal);
+  analogWrite(txBluePin, bVal);
+  analogWrite(txPumpPin, pVal);
+  if (time - lastPrint > 100) {
+    lastPrint = time;
+    printState();
+  }
+  delay(30);
 }
